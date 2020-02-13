1f1b126f5d7c65e1568d295b1f7d608c9bba8f58
JoesphxNadja <59183260+JoesphxNadja@users.noreply.github.com>
2020-02-13 08:10:41 -0800

    Add files via upload

--- Casino-Game.js
+++ Casino-Game.js
@@ -1,2 +1,17 @@
+//    WELCOME TO BLACKJACK!
+
 const numberCards = [2, 3, 4, 5, 6, 7, 8, 9, 10];
 const faceCards = ['Jack', 'Queen', 'King', 'Ace'];
+
+let myWallet = 100;
+
+
+function greeting(myWallet) {
+    if (confirm("Do you want to play some Blackjack?")) {
+        prompt(`great! You have $${myWallet} in your wallet.\nHow much would you like to bet?`);
+  } else {
+        prompt("I'm sorry to hear that :(");
+  }
+}
+
+console.log(greeting(myWallet)); 
